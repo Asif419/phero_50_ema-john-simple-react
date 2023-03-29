3a3fc68b452cc7a1539852b8d0301f31e114f49e
Asif419 <asshah419@gmail.com>
2023-03-29 12:11:02 +0600

    cart loaded from localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({cart}) => {
-  const {length} = cart;
-  const totalPrice = cart.reduce((previous, current) => previous + current.price, 0);
+const Cart = ({ cart }) => {
   // different way to get total
   let totalShipping = 0;
+  let quantity = 0;
+  let totalPrice = 0;
+
   for (const product of cart) {
+    totalPrice += product.price * product.quantity;
     totalShipping += product.shipping;
+    quantity += product.quantity;
   }
   const tax = totalPrice * 7 / 100;
   const grandTotal = totalPrice + totalShipping + tax;
+  
   return (
     <div className='cart'>
       <h4>Order Summary</h4>
-      <p>Selected Items: {length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -6,7 +6,7 @@ import './Shop.css';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('products.json')
       .then(res => res.json())
@@ -27,11 +27,24 @@ const Shop = () => {
         savedCart.push(addedProduct);
       }
       // step 5: set the cart
+      setCart(savedCart); // this is used because in every web page load we can get previous saved cart values from localStorage
     }
   }, [products])
 
   const handleAddToCart = (product) => {
-    setCart([...cart, product]);
+    let newCart = [];
+    const exists = cart.find(pd => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
+    else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
+    setCart(newCart); // set the quantity in every selected product
+    // setCart here added product to localStorage after click on addToCart
     addToDb(product.id);
   }
   return (
